9a628a1de4df6f5ea50e77206286f8839924516a
sylvain-malbon <sylvain.malbon@laplateforme.io>
2025-11-05 12:14:10 +0100

    Création structure globale complète Début codage header.php

---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<header class="header">
+    <div class="container">
+        <nav class="navbar">
+            <a href="index.php" class="logo" title="Accueil">
+                <img src=" assets/images/logo.png" alt="Logo du site">
+            </a>
+            <ul class="nav-list">
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="inscription.php">Inscription</a></li>
+                <li><a href="commentaire.php">Laisser un commentaire</a></li>
+                <li><a href="livre-or.php">Livre d'or</a></li>
+                <li><a href="connexion.php">Connexion</a></li>
