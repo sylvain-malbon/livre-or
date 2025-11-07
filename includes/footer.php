7e297889a477f117674eff9080b4d47589007497
sylvain-malbon <sylvain.malbon@laplateforme.io>
2025-11-07 11:17:57 +0100

    Footer : texte tout en minuscules CSS : 90%

--- includes/footer.php
+++ includes/footer.php
@@ -5,14 +5,14 @@
             <ul class="nav-list">
 
                 <?php if (isset($_SESSION['login'])) : ?>
-                    <li><a href="/pages/profil.php">Modifier mon profil</a></li>
-                    <li><a href="/pages/commentaire.php">Laisser un commentaire</a></li>
-                    <li><a href="index.php">Déconnexion</a></li>
+                    <li><a href="/pages/profil.php">modifier mon profil</a></li>
+                    <li><a href="/pages/commentaire.php">laisser un commentaire</a></li>
+                    <li><a href="index.php">déconnexion</a></li>
                 <?php else : ?>
-                    <li><a href="index.php">Accueil</a></li>
-                    <li><a href="/pages/inscription.php">S'inscrire</a></li>
-                    <li><a href="/pages/livre-or.php">Consulter le livre d'or</a></li>
-                    <li><a href="/pages/connexion.php" title="login">Se connecter</a></li>
+                    <li><a href="index.php">accueil</a></li>
+                    <li><a href="/pages/inscription.php">s'inscrire</a></li>
+                    <li><a href="/pages/livre-or.php">consulter le livre d'or</a></li>
+                    <li><a href="/pages/connexion.php" title="login">se connecter</a></li>
                 <?php endif; ?>
 
             </ul>
