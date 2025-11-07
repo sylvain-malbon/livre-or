114bfc12976032c8b5319c281bb9f175ee8c9b13
sylvain-malbon <sylvain.malbon@laplateforme.io>
2025-11-07 14:07:24 +0100

    Remplacement de S'inscrire par Laissez un commentaire ; Redirection vers Connexion.php de Laissez un commentaire (Header) ; Séparateurs Header et Footer avec styles différents ; CSS 95%.

--- includes/footer.php
+++ includes/footer.php
@@ -6,12 +6,17 @@
 
                 <?php if (isset($_SESSION['login'])) : ?>
                     <li><a href="/pages/profil.php">modifier mon profil</a></li>
+                    <li class="separateur">|</li>
                     <li><a href="/pages/commentaire.php">laisser un commentaire</a></li>
+                    <li class="separateur">|</li>
                     <li><a href="index.php">déconnexion</a></li>
                 <?php else : ?>
                     <li><a href="index.php">accueil</a></li>
-                    <li><a href="/pages/inscription.php">s'inscrire</a></li>
+                    <li class="separateur">|</li>
+                    <li><a href="/pages/inscription.php">laisser un commentaire</a></li>
+                    <li class="separateur">|</li>
                     <li><a href="/pages/livre-or.php">consulter le livre d'or</a></li>
+                    <li class="separateur">|</li>
                     <li><a href="/pages/connexion.php" title="login">se connecter</a></li>
                 <?php endif; ?>
 
--- includes/header.php
+++ includes/header.php
@@ -8,12 +8,17 @@
 
                 <?php if (isset($_SESSION['login'])) : ?>
                     <li><a href="/pages/profil.php">Modifier mon profil</a></li>
+                    <li class="separateur">|</li>
                     <li><a href="/pages/commentaire.php">Laisser un commentaire</a></li>
+                    <li class="separateur">|</li>
                     <li><a href="index.php">Déconnexion</a></li>
                 <?php else : ?>
                     <li><a href="index.php">Accueil</a></li>
-                    <li><a href="/pages/inscription.php">S'inscrire</a></li>
+                    <li class="separateur">|</li>
+                    <li><a href="/pages/connexion.php">Laisser un commentaire</a></li>
+                    <li class="separateur">|</li>
                     <li><a href="/pages/livre-or.php">Consulter le livre d'or</a></li>
+                    <li class="separateur">|</li>
                     <li><a href="/pages/connexion.php" title="login">Se connecter</a></li>
                 <?php endif; ?>
 
