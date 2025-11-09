fa1a67eed04ba0d0428299cb1dc50d76e9501169
sylvain-malbon <sylvain.malbon@laplateforme.io>
2025-11-09 22:05:42 +0100

    Container : effacé dans Header et Footer. CSS : - Refonte manuelle (moins esthétique) de style.css - Essai avec Sonnet 4.5 (style-sonnet45.css)

--- includes/footer.php
+++ includes/footer.php
@@ -1,26 +1,26 @@
 <footer class="footer">
-    <div class="container">
-        <nav class="footer-navbar">
 
-            <ul class="nav-list">
+    <nav class="footer-navbar">
 
-                <?php if (isset($_SESSION['login'])) : ?>
-                    <li><a href="<?= BASE_PATH ?>pages/profil.php">modifier mon profil</a></li>
-                    <li class="separateur">|</li>
-                    <li><a href="<?= BASE_PATH ?>pages/commentaire.php">laisser un commentaire</a></li>
-                    <li class="separateur">|</li>
-                    <li><a href="<?= BASE_PATH ?>index.php" title="déconnexion">déconnexion</a></li>
-                <?php else : ?>
-                    <li><a href="<?= BASE_PATH ?>index.php" title="accueil">accueil</a></li>
-                    <li class="separateur">|</li>
-                    <li><a href="<?= BASE_PATH ?>pages/inscription.php" title="inscription">s'inscrire pour commenter</a></li>
-                    <li class="separateur">|</li>
-                    <li><a href="<?= BASE_PATH ?>pages/livre-or.php" title="livre d'or">consulter le livre d'or</a></li>
-                    <li class="separateur">|</li>
-                    <li><a href="<?= BASE_PATH ?>pages/connexion.php" title="connexion">se connecter</a></li>
-                <?php endif; ?>
+        <ul class="nav-list">
 
-            </ul>
-        </nav>
-    </div>
+            <?php if (isset($_SESSION['login'])) : ?>
+                <li><a href="<?= BASE_PATH ?>pages/profil.php">modifier mon profil</a></li>
+                <li class="separateur">|</li>
+                <li><a href="<?= BASE_PATH ?>pages/commentaire.php">laisser un commentaire</a></li>
+                <li class="separateur">|</li>
+                <li><a href="<?= BASE_PATH ?>index.php" title="déconnexion">déconnexion</a></li>
+            <?php else : ?>
+                <li><a href="<?= BASE_PATH ?>index.php" title="accueil">accueil</a></li>
+                <li class="separateur">|</li>
+                <li><a href="<?= BASE_PATH ?>pages/inscription.php" title="inscription">s'inscrire pour commenter</a></li>
+                <li class="separateur">|</li>
+                <li><a href="<?= BASE_PATH ?>pages/livre-or.php" title="livre d'or">consulter le livre d'or</a></li>
+                <li class="separateur">|</li>
+                <li><a href="<?= BASE_PATH ?>pages/connexion.php" title="connexion">se connecter</a></li>
+            <?php endif; ?>
+
+        </ul>
+    </nav>
+
 </footer>
--- includes/header.php
+++ includes/header.php
@@ -1,34 +1,34 @@
 <header class="header">
-    <div class="container">
-        <nav class="header-navbar">
-            <a href="<?= BASE_PATH ?>index.php" class="logo" title="Accueil">
-                <img src="<?= BASE_PATH ?>assets/images/logo_livre-or.png" alt="Logo du site">
-            </a>
-            <ul class="nav-list">
 
-                <?php if (isset($_SESSION['login'])) : ?>
-                    <li><a href="<?= BASE_PATH ?>index.php" title="accueil"><span class="initiale">A</span>ccueil</a></li>
-                    <li class="separateur">|</li>
-                    <li><a href="<?= BASE_PATH ?>pages/profil.php" title="profil">
-                            <span class="initiale">M</span>odifier mon profil
-                        </a></li>
-                    <li class="separateur">|</li>
-                    <li><a href="<?= BASE_PATH ?>pages/commentaire.php" title="commenter"><span class="initiale">L</span>aisser un commentaire</a></li>
-                    <li class="separateur">|</li>
-                    <li><a href="<?= BASE_PATH ?>pages/livre-or.php" title="livre d'or"><span class="initiale">C</span>onsulter le <span class="logotype">Livre d'or<span></a></li>
-                    <li class="separateur">|</li>
-                    <li><a href="<?= BASE_PATH ?>pages/deconnexion.php" title="déconnexion"><span class="initiale">D</span>éconnexion</a></li>
-                <?php else : ?>
-                    <li><a href="<?= BASE_PATH ?>index.php" title="accueil"><span class="initiale">A</span>ccueil</a></li>
-                    <li class="separateur">|</li>
-                    <li><a href="<?= BASE_PATH ?>pages/inscription.php" title="register"><span class="initiale">S</span><span class="apostrophe">'</span>inscrire pour commenter</a></li>
-                    <li class="separateur">|</li>
-                    <li><a href="<?= BASE_PATH ?>pages/livre-or.php" title="livre d'or"><span class="initiale">C</span>onsulter le <span class="logotype">Livre d'or<span></a></li>
-                    <li class="separateur">|</li>
-                    <li><a href="<?= BASE_PATH ?>pages/connexion.php" title="connexion"><span class="initiale">S</span>e connecter</a></li>
-                <?php endif; ?>
+    <nav class="header-navbar">
+        <a href="<?= BASE_PATH ?>index.php" class="logo" title="Accueil">
+            <img src="<?= BASE_PATH ?>assets/images/logo_livre-or.png" alt="Logo du site">
+        </a>
+        <ul class="nav-list">
 
-            </ul>
-        </nav>
-    </div>
+            <?php if (isset($_SESSION['login'])) : ?>
+                <li><a href="<?= BASE_PATH ?>index.php" title="accueil"><span class="initiale">A</span>ccueil</a></li>
+                <li class="separateur">|</li>
+                <li><a href="<?= BASE_PATH ?>pages/profil.php" title="profil">
+                        <span class="initiale">M</span>odifier mon profil
+                    </a></li>
+                <li class="separateur">|</li>
+                <li><a href="<?= BASE_PATH ?>pages/commentaire.php" title="commenter"><span class="initiale">L</span>aisser un commentaire</a></li>
+                <li class="separateur">|</li>
+                <li><a href="<?= BASE_PATH ?>pages/livre-or.php" title="livre d'or"><span class="initiale">C</span>onsulter le <span class="logotype">Livre d'or<span></a></li>
+                <li class="separateur">|</li>
+                <li><a href="<?= BASE_PATH ?>pages/deconnexion.php" title="déconnexion"><span class="initiale">D</span>éconnexion</a></li>
+            <?php else : ?>
+                <li><a href="<?= BASE_PATH ?>index.php" title="accueil"><span class="initiale">A</span>ccueil</a></li>
+                <li class="separateur">|</li>
+                <li><a href="<?= BASE_PATH ?>pages/inscription.php" title="register"><span class="initiale">S</span><span class="apostrophe">'</span>inscrire pour commenter</a></li>
+                <li class="separateur">|</li>
+                <li><a href="<?= BASE_PATH ?>pages/livre-or.php" title="livre d'or"><span class="initiale">C</span>onsulter le <span class="logotype">Livre d'or<span></a></li>
+                <li class="separateur">|</li>
+                <li><a href="<?= BASE_PATH ?>pages/connexion.php" title="connexion"><span class="initiale">S</span>e connecter</a></li>
+            <?php endif; ?>
+
+        </ul>
+    </nav>
+
 </header>
