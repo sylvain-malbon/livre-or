403017de48684c2d923a685073c5dd683e3f82ae
sylvain-malbon <sylvain.malbon@laplateforme.io>
2025-11-10 13:37:04 +0100

    Footer : onglets tout en minuscules même déconnecté

--- includes/footer.php
+++ includes/footer.php
@@ -17,13 +17,13 @@
                 <li class="separateur">|</li>
                 <li><a href="<?= BASE_PATH ?>pages/deconnexion.php" title="déconnexion">déconnexion</a></li>
             <?php else : ?>
-                <li><a href="<?= BASE_PATH ?>index.php" title="accueil">Accueil</a></li>
+                <li><a href="<?= BASE_PATH ?>index.php" title="accueil">accueil</a></li>
                 <li class="separateur">|</li>
-                <li><a href="<?= BASE_PATH ?>pages/inscription.php" title="register">S'inscrire pour commenter</a></li>
+                <li><a href="<?= BASE_PATH ?>pages/inscription.php" title="register">s'inscrire pour commenter</a></li>
                 <li class="separateur">|</li>
-                <li><a href="<?= BASE_PATH ?>pages/livre-or.php" title="livre d'or">Consulter le livre d'or</a></li>
+                <li><a href="<?= BASE_PATH ?>pages/livre-or.php" title="livre d'or">consulter le livre d'or</a></li>
                 <li class="separateur">|</li>
-                <li><a href="<?= BASE_PATH ?>pages/connexion.php" title="connexion">Se connecter</a></li>
+                <li><a href="<?= BASE_PATH ?>pages/connexion.php" title="connexion">se connecter</a></li>
             <?php endif; ?>
 
         </ul>
